dc98996879f52148c0573c5d39687fad29f812c1
Tawmy <kersting.tom@gmail.com>
2017-06-11 11:35:58 +0200

    Add confirmation modal

---
+++ script.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+	$('#join-button').click(function(){
+		$('.modal').addClass("modal-show");
+		$('section').addClass("blur");
+		$('.bukoya').addClass("blur");
+	});
+	$('section').mouseup(function(){
+		$('.modal').removeClass("modal-show");
+		$('section').removeClass("blur");
+		$('.bukoya').removeClass("blur");
+	});
+});
