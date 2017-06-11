b1f0e9e0b75792dac41080c34cf2616109f830d9
Tawmy <kersting.tom@gmail.com>
2017-06-11 14:18:48 +0200

    Minor code refinement

--- script.js
+++ script.js
@@ -1,14 +1,12 @@
 $(document).ready(function(){
 	$('#join-button').click(function(){
 		$('.modal').addClass("modal-show");
-		$('section').addClass("blur");
-		$('.bukoya').addClass("blur");
+		$('section,.bukoya').addClass("blur");
 		$('.header-link').css({"pointer-events" : "none"});
 	});
 	$('section').mouseup(function(){
 		$('.modal').removeClass("modal-show");
-		$('section').removeClass("blur");
-		$('.bukoya').removeClass("blur");
+		$('section,.bukoya').removeClass("blur");
 		$('.header-link').css({"pointer-events" : "initial"});
 	});
 });
