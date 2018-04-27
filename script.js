37f816ea25280e61c0c8ed99cde027c9486258ab
Tawmy <kersting.tom@gmail.com>
2018-04-27 14:12:16 +0200

    Code cleanup and design adjustments

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 $(document).ready(function(){
-	$('#join-button').click(function(){
-		$('.modal').addClass("modal-show");
-		$('section,.bukoya').addClass("blur");
-		$('.header-link').css({"pointer-events" : "none"});
-		$('html,body').css({"overflow" : "hidden"});
-	});
-	$('section').mouseup(function(){
-		$('.modal').removeClass("modal-show");
-		$('section,.bukoya').removeClass("blur");
-		$('.header-link').css({"pointer-events" : "initial"});
-		$('html,body').css({"overflow" : "initial"});
-	});
+    $('#join-button').click(function(){
+        $('.modal').addClass("modal-show");
+        $('section,.bukoya').addClass("blur");
+        $('.header-link').css({"pointer-events" : "none"});
+        $('html,body').css({"overflow" : "hidden"});
+    });
+    $('section').mouseup(function(){
+        $('.modal').removeClass("modal-show");
+        $('section,.bukoya').removeClass("blur");
+        $('.header-link').css({"pointer-events" : "initial"});
+        $('html,body').css({"overflow" : "initial"});
+    });
 });
