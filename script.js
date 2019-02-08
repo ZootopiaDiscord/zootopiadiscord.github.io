98a051a98b3d6dffca310aa9b3d0b1ed9e85d148
Tawmy <kersting.tom@gmail.com>
2019-02-08 09:46:33 +0100

    Code format

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
-$(document).ready(function(){
-    $('#join-button').click(function(){
+$(document).ready(function () {
+    $('#join-button').click(function () {
         $('.modal').addClass("modal-show");
         $('section,.bukoya').addClass("blur");
-        $('.header-link').css({"pointer-events" : "none"});
-        $('html,body').css({"overflow" : "hidden"});
+        $('.header-link').css({ "pointer-events": "none" });
+        $('html,body').css({ "overflow": "hidden" });
     });
-    $('section').mouseup(function(){
+    $('section').mouseup(function () {
         $('.modal').removeClass("modal-show");
         $('section,.bukoya').removeClass("blur");
-        $('.header-link').css({"pointer-events" : "initial"});
-        $('html,body').css({"overflow" : "initial"});
+        $('.header-link').css({ "pointer-events": "initial" });
+        $('html,body').css({ "overflow": "initial" });
     });
-    $('h1').click(function(){
+    $('h1').click(function () {
         $(this).parent("section").toggleClass("section-collapse");
         $(this).next("article").toggleClass("article-hide");
     });
