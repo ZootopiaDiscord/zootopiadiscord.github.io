a152f76f21b93830e5bc5ff5c88b162ebf7a5e09
Tawmy <kersting.tom@gmail.com>
2019-02-14 20:06:23 +0100

    Design adjustments to header
    Wave effect on bottom of header + round join button
    
    I blame Jul for these

--- script.js
+++ script.js
@@ -2,12 +2,14 @@ $(document).ready(function () {
     $('#join-button').click(function () {
         $('.modal').addClass("modal-show");
         $('section,.bukoya').addClass("blur");
+        $('.header').addClass("header-removeClip");
         $('.header-link').css({ "pointer-events": "none" });
         $('html,body').css({ "overflow": "hidden" });
     });
     $('section').mouseup(function () {
         $('.modal').removeClass("modal-show");
         $('section,.bukoya').removeClass("blur");
+        $('.header').removeClass("header-removeClip");
         $('.header-link').css({ "pointer-events": "initial" });
         $('html,body').css({ "overflow": "initial" });
     });
