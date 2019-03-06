d180197c2c6004fb453333fd12ef2c7f04704ae2
Tawmy <kersting.tom@gmail.com>
2019-03-06 11:53:31 +0100

    Use main image resources + load invite dynamically
    also auto format code

--- legacy/script.js
+++ legacy/script.js
@@ -1,14 +1,23 @@
-$(document).ready(function(){
-    $('#join-button').click(function(){
+$(document).ready(function () {
+    $('#join-button').click(function () {
         $('.modal').addClass("modal-show");
         $('section,.bukoya').addClass("blur");
-        $('.header-link').css({"pointer-events" : "none"});
-        $('html,body').css({"overflow" : "hidden"});
+        $('.header-link').css({ "pointer-events": "none" });
+        $('html,body').css({ "overflow": "hidden" });
     });
-    $('section').mouseup(function(){
+    $('section').mouseup(function () {
         $('.modal').removeClass("modal-show");
         $('section,.bukoya').removeClass("blur");
-        $('.header-link').css({"pointer-events" : "initial"});
-        $('html,body').css({"overflow" : "initial"});
+        $('.header-link').css({ "pointer-events": "initial" });
+        $('html,body').css({ "overflow": "initial" });
+    });
+
+    /* from new site, avoids higher maintenance */
+    $.get('/invite.txt', function (data) {
+        $(".modal").append(
+            '<a href="' + data + '" class="modal-link">\
+                JOIN\
+            </a>'
+        );
     });
 });
