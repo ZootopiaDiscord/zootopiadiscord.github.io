1b306ec8fad663f670d896cb4af7ad68aa501f8c
Tawmy <kersting.tom@gmail.com>
2019-03-06 12:15:11 +0100

    Rough backport of new modal

--- legacy/script.js
+++ legacy/script.js
@@ -1,17 +1,4 @@
 $(document).ready(function () {
-    $('#join-button').click(function () {
-        $('.modal').addClass("modal-show");
-        $('section,.bukoya').addClass("blur");
-        $('.header-link').css({ "pointer-events": "none" });
-        $('html,body').css({ "overflow": "hidden" });
-    });
-    $('section').mouseup(function () {
-        $('.modal').removeClass("modal-show");
-        $('section,.bukoya').removeClass("blur");
-        $('.header-link').css({ "pointer-events": "initial" });
-        $('html,body').css({ "overflow": "initial" });
-    });
-
     /* from new site, avoids higher maintenance */
     $.get('/invite.txt', function (data) {
         $(".modal").append(
@@ -19,5 +6,18 @@ $(document).ready(function () {
                 JOIN\
             </a>'
         );
+    });
+
+    /* open modal */
+    $('#join-button').click(function () {
+        $('.modal-container').addClass("modal-container-show");
+        $('html,body').css({ "overflow": "hidden" });
+    });
+
+    /* close modal */
+    $('.modal-container').click(function (e) {
+        if (e.target !== e.currentTarget) return;
+        $('.modal-container').removeClass("modal-container-show");
+        $('html,body').css({ "overflow": "visible" });
     });
 });
