6210884117373f425d80384cb394b0be4bd2f59a
Tawmy <kersting.tom@gmail.com>
2019-03-06 16:04:57 +0100

    Load rules from rules*.txt

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ $(document).ready(function () {
 
     window.scrollTo(0, 0);
 
+    generateRules();
+
     /* load Discord invite link from invite.txt */
     $.get('invite.txt', function (data) {
         $(".modal").append(
@@ -84,6 +86,38 @@ function scrollIndicator() {
                 scrollTop: $('.rules').offset().top
             }, scrollDuration);
         }
+    });
+}
+
+function generateRules() {
+    $.get('rules.txt', function (data) {
+        let rawRules = data.split('\n');
+        let rules = "";
+        for (let index = 0; index < rawRules.length; index++) {
+            if (rawRules[index].length < 2) {
+                rules += "</ol>"
+            }
+            else if (rawRules[index].substring(0, 1) == ">") {
+                rules += "<li>" + rawRules[index].substring(1) + "</li><ol>";
+            }
+            else {
+                rules += "<li>" + rawRules[index] + "</li>"
+            }
+        }
+        rules += "</ol>"
+        $("#ul-rules").html(rules);
+    });
+    $.get('rules_other.txt', function (data) {
+        let rulesOther = "<li>" + data + "</li>";
+        $("#ul-other").html(rulesOther);
+    });
+    $.get('rules_zpd.txt', function (data) {
+        let rawRulesZPD = data.split('\n');
+        let rulesZPD = "";
+        for (let index = 0; index < rawRulesZPD.length; index++) {
+            rulesZPD += "<li>" + rawRulesZPD[index] + "</li>"
+        }
+        $("#ul-zpd").html(rulesZPD);
     });
 }
 
