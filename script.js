18332aaadb0a742e97dfb160508bbb4e80cd89c6
Tawmy <kersting.tom@gmail.com>
2019-03-07 11:25:47 +0100

    Generate channel list from channels.txt

--- script.js
+++ script.js
@@ -2,9 +2,6 @@ $(document).ready(function () {
 
     window.scrollTo(0, 0);
 
-    generateRules();
-    generateMods();
-
     /* load Discord invite link from invite.txt */
     $.get('invite.txt', function (data) {
         $(".modal").append(
@@ -90,55 +87,6 @@ function scrollIndicator() {
     });
 }
 
-function generateRules() {
-    $.get('rules.txt', function (data) {
-        let rawRules = data.split('\n');
-        let rules = "";
-        for (let index = 0; index < rawRules.length; index++) {
-            if (rawRules[index].length < 2) {
-                rules += "</ol>"
-            }
-            else if (rawRules[index].substring(0, 1) == ">") {
-                rules += "<li>" + rawRules[index].substring(1) + "</li><ol>";
-            }
-            else {
-                rules += "<li>" + rawRules[index] + "</li>"
-            }
-        }
-        rules += "</ol>"
-        $("#ul-rules").html(rules);
-    });
-    $.get('rules_other.txt', function (data) {
-        let rulesOther = "<li>" + data + "</li>";
-        $("#ul-other").html(rulesOther);
-    });
-    $.get('rules_zpd.txt', function (data) {
-        let rawRulesZPD = data.split('\n');
-        let rulesZPD = "";
-        for (let index = 0; index < rawRulesZPD.length; index++) {
-            rulesZPD += "<li>" + rawRulesZPD[index] + "</li>"
-        }
-        $("#ul-zpd").html(rulesZPD);
-    });
-}
-
-function generateMods() {
-    $.get('mods.txt', function (data) {
-        let rawMods = data.split('\n');
-        let mods = "";
-        for (let index = 0; index < rawMods.length; index += 2) {
-            mods += '<div class="mod"><a href="http://discordapp.com/users/';
-            mods += rawMods[index + 1];
-            mods += '"><img src="img/avatars/';
-            mods += rawMods[index];
-            mods += '.jpg"></a><h2>'
-            mods += rawMods[index];
-            mods += '</h2></div>'
-        }
-        $(".mods").children("article").html(mods);
-    });
-}
-
 function modal() {
     /* open modal */
     $('#join-button, #bukoya').click(function () {
@@ -156,7 +104,7 @@ function modal() {
 
 function channels() {
     /* collapse / open channel category */
-    $('.channels article ul li:first-child').click(function () {
+    $('.channels').on("click", "ul li:first-child", function () {
         $(this).parent("ul").children("li").not(".channels article ul li:first-child").toggleClass("channelCategory-hide");
         $(this).parent("ul").children("li:first-child").toggleClass("channelCategory-closed");
     });
