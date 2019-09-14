9cb988e971ad1062b54f279bbb0061287cb9c877
Tawmy <kersting.tom@gmail.com>
2019-09-14 12:44:35 +0200

    Update mods design

--- generateSections.js
+++ generateSections.js
@@ -155,9 +155,9 @@ function generateMods() {
             mods += rawMods[index + 1];
             mods += '.jpg" alt="';
             mods += rawMods[index];
-            mods += '"></a><h2>';
+            mods += '"><h2>';
             mods += rawMods[index];
-            mods += '</h2></div>'
+            mods += '</h2></a></div>'
         }
         $(".mods").children("article").html(mods);
     });
